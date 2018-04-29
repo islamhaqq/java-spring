a58523beffd2cb96401a992933d0eac8c3f72bc7
Syed Aman <sna274@nyu.edu>
2018-04-29 15:47:54 -0500

    update files

--- my-app/src/main/java/com/syedaman/hello/Greeter.java
+++ my-app/src/main/java/com/syedaman/hello/Greeter.java
@@ -4,4 +4,8 @@ public class Greeter {
   public String sayHello() {
     return "Hello world!";
   }
+
+  public String introduce(String name, int age) {
+    return String.format("My name is %s and I am %d years old.", name, age);
+  }
 }
--- my-app/src/main/java/com/syedaman/hello/HelloWorld.java
+++ my-app/src/main/java/com/syedaman/hello/HelloWorld.java
@@ -1,12 +1,7 @@
 package com.syedaman.hello;
 
-import org.joda.time.LocalTime;
-
 public class HelloWorld {
   public static void main( String[] args ) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current time is: " + currentTime);
-
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
